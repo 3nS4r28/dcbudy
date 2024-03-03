cf7797585edebefcb12222576d51ea8f597ab642
3nS4r28 <161999137+3nS4r28@users.noreply.github.com>
2024-03-03 15:00:11 +0300

    premium.jsx

--- pages/premium.jsx
+++ pages/premium.jsx
@@ -5,6 +5,8 @@ export default function Pramium() {
             <h1 className="font-semibold text-3xl text-white/75">DcBudy Premium</h1>
   <div className="animateHeader text-white text-opacity-40 mt-5">
         <p>⊳ To use some of the commands of the DcBudy bot you need to buy Premium. To Buy <a className="text-white" href="https://discord.gg/QyvUUQ8rNK">CLİCK HERE</a> </p>
+
+        <p>⊳ <a className="text-white">Supported Money Types</a> </p>
         <div className="py-3"></div>
         <p>⊳ ₺ (TL/TRY) </p>
         <div className="py-3"></div>
