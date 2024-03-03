d20b2692a9190ae9e2f13c1694484559975e7092
3nS4r28 <161999137+3nS4r28@users.noreply.github.com>
2024-03-03 14:32:28 +0300

    premium.jsx

--- pages/premium.jsx
+++ pages/premium.jsx
@@ -1,20 +1,8 @@
 export default function Pramium() {
 
     return (
-              <>
-            <div className="w-full my-10">
-            <div className="flex w-full justify-center">
-      <img width="400" src="/img/bck4.png" />
-      </div>
-                <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
-                    <i className="fal fa-stars text-amber-400 mr-2" />
-                    Partners
-                </h1>
-            </div>
 
-            <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div>
-            <h1 className="font-semibold text-3xl text-white/75">DcBudy Bot Premium</h1>
+        <h1 className="font-semibold text-3xl text-white/75">DcBudy Bot Premium</h1>
   <div className="animateHeader text-white text-opacity-40 mt-5">
         <p>⊳ To use some of the commands of the DcBudy bot you need to buy Premium. To Buy <a className="text-white" href="https://discord.gg/QyvUUQ8rNK">CLİCK HERE</a> </p>
         <div className="py-3"></div>
