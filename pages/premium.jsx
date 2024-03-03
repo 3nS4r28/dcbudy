c8c064c41f7d715ea1cfbf056a31f166b1c53c31
3nS4r28 <161999137+3nS4r28@users.noreply.github.com>
2024-03-03 14:34:37 +0300

    premium.jsx

--- pages/premium.jsx
+++ pages/premium.jsx
@@ -1,13 +1,1 @@
-export default function Pramium() {
 
-    return (
-
-        <h1 className="font-semibold text-3xl text-white/75">DcBudy Bot Premium</h1>
-  <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use some of the commands of the DcBudy bot you need to buy Premium. To Buy <a className="text-white" href="https://discord.gg/QyvUUQ8rNK">CLİCK HERE</a> </p>
-        <div className="py-3"></div>
-    
-        </div>
-            </div>
-    );
-};
