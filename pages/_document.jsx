a4063aca97105e29761269436d1f676d02ec14d5
3nS4r28 <161999137+3nS4r28@users.noreply.github.com>
2024-03-03 12:02:32 +0300

    _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -14,11 +14,11 @@ class MyDocument extends Document {
             content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
           />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta name="twitter:site" content="@DcBudy" />
+          <meta name="twitter:creator" content="@DcBudy" />
+          <meta property="og:url" content="https://dcbudy.vercel.app/" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="DcBudy" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -33,7 +33,7 @@ class MyDocument extends Document {
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="DcBudy"
           />
           <meta name="theme-color" content="#087ddd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
