71faf226150a720efd22a83dd925f69fe9fb7eb2
3nS4r28 <161999137+3nS4r28@users.noreply.github.com>
2024-03-03 12:39:12 +0300

    tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -2,19 +2,21 @@ export default function Tos() {
   return (
     <div>
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
-      <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
+      <p className="text-sm text-white/30 mb-1">Last Update: 03/03/2024 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the MusicMaker bot for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳ Every discord server can use the DcBudy bot for free, it is completely prohibited to sell or market for a fee.</p>
 <div className="py-3"></div>
 <p>⊳ You cannot use bot commands to harm the bot.</p>
 <div className="py-3"></div>
 <p>⊳ You may not use open source codes to harm other servers.</p>
 <div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
+<p>⊳ You cannot sell or market open source code on other platforms. Except Official Server!</p>
 <div className="py-3"></div>
 <p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
 <div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot at certain intervals.</p>
+<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/1205170061413982279/vote">DcBudy</a> bot at certain intervals.</p>
+</div>
+<p>⊳ To use certain discord bot commands, you need to premium for the <a className="text-white" href="https://discord.gg/QyvUUQ8rNK">Buy a Premium</a> </p>
 </div>
     </div>
   );
