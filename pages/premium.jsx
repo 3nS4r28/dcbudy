55edc37f29c5aee1f8c4ce7bf5259c543881867e
3nS4r28 <161999137+3nS4r28@users.noreply.github.com>
2024-03-03 14:31:08 +0300

    premium.jsx

--- pages/premium.jsx
+++ pages/premium.jsx
@@ -1,6 +1,18 @@
 export default function Pramium() {
 
     return (
+              <>
+            <div className="w-full my-10">
+            <div className="flex w-full justify-center">
+      <img width="400" src="/img/bck4.png" />
+      </div>
+                <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
+                    <i className="fal fa-stars text-amber-400 mr-2" />
+                    Partners
+                </h1>
+            </div>
+
+            <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
         <div>
             <h1 className="font-semibold text-3xl text-white/75">DcBudy Bot Premium</h1>
   <div className="animateHeader text-white text-opacity-40 mt-5">
