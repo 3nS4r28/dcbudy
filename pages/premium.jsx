80fe206d1302c7fbd718a8b9da98e9ebc38fa12e
3nS4r28 <161999137+3nS4r28@users.noreply.github.com>
2024-03-03 14:51:25 +0300

    premium.jsx

--- pages/premium.jsx
+++ pages/premium.jsx
@@ -1,91 +1,23 @@
-import { Menu, Transition } from "@headlessui/react";
-import Link from "next/link";
+export default function Pramium() {
 
-export default function Premium() {
-const premiumcommands = [
-  "/autoplay",
-  "/back",
-  "/statistic",
-  "/channel",
-  "/clear",
-  "/dj",
-  "/filter",
-  "/help",
-  "/language",
-  "/loop",
-  "/nowplaying",
-  "/pause",
-  "/ping",
-  "/play",
-  "/playlist",
-  "/queue",
-  "/resume",
-  "/save",
-  "/search",
-  "/seek",
-  "/servers",
-  "/shuffle",
-  "/skip",
-  "/stop",
-  "/time",
-  "/volume"
-  "/slm"
-];
-
-  return (
-    <>
-      <div>
-      <div className="flex w-full justify-center">
-      <img width="500" src="/img/bck2.png" />
-      </div>
-        <p className="text-xl font-medium text-white">
-          <i className="fal fa-cogs text-amber-400 mr-2" />
-         Bot Commands
-        </p>
-        <p className="text-white text-sm text-opacity-50 mb-5">
-        You can get information about the commands of the DcBudy Bot.
-        </p>
-
+    return (
         <div>
-          <Menu as="div" className="w-full relative inline-block text-left">
-          <div>
-              <Menu.Button className="left-0 transition w-full duration-200 hover:bg-opacity-50 bg-black bg-opacity-30 text-white focus:text-amber-400 rounded-xl border border-white/10 focus:border-amber-400 focus:outline-none py-4 px-6 flex justify-between items-center">
-                <div className="flex items-center gap-x-1 text-white text-opacity-60">
-                 
-                    <>
-                      <i className="fa fa-hashtag text-white text-opacity-20" />
-                     View Commands List
-                    </>
-                
-                </div>
-              </Menu.Button>
+            <h1 className="font-semibold text-3xl text-white/75">DcBudy Premium</h1>
+  <div className="animateHeader text-white text-opacity-40 mt-5">
+        <p>⊳ To use some of the commands of the DcBudy bot you need to buy Premium. To Buy <a className="text-white" href="https://discord.gg/QyvUUQ8rNK">CLİCK HERE</a> </p>
+        <div className="py-3"></div>
+    
+              <h1 className="font-semibold text-3xl text-white/75">Supported Currencies</h1>
+    <div className="animateHeader text-white text-opacity-40 mt-5">
+        <p>⊳ ₺ (TL/TRY) </p>
+        <div className="py-3"></div>
+        <p>⊳ $ (Dollar) </p>
+        <div className="py-3"></div>
+        <p>⊳ € (Euro) </p>
+        <div className="py-3"></div>
+
+      
+        </div>
             </div>
-
-            <Transition
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95">
-              <Menu.Items className="z-1 custom-scroll absolute left-0 w-full mt-2 origin-top-right bg-black overflow-auto max-h-60 border border-black border-opacity-20 bg-opacity-95 rounded-lg p-1">
-                <div className="px-1 py-1 w-full">
-
-            {commands.map(mm => (
-
-                <Menu.Item>
-                    <button className={`w-full rounded-lg p-2 cursor-pointer text-white flex items-center space-x-3 transition-all duration-200`}>
-                    <>
-                          <i className="fa-spin text-xl text-white text-opacity-20" />
-                         {mm}</>
-                    </button>
-                    </Menu.Item>
-            ))}
-              
-        </div>
-              </Menu.Items>
-            </Transition>
-          </Menu>
-        </div>
-
-        <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
+    );
+};
