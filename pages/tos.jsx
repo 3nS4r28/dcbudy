cb37d4a9ac5432df6ebd8f307fdc9be1f6ef2522
3nS4r28 <161999137+3nS4r28@users.noreply.github.com>
2024-03-03 13:33:06 +0300

    tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -1,7 +1,7 @@
 export default function Tos() {
   return (
     <div>
-     <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
+     <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services for DcBudy</h2>
       <p className="text-sm text-white/30 mb-1">Last Update: 03/03/2024 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
 <p>⊳ Every discord server can use the DcBudy bot for free, it is completely prohibited to sell or market for a fee.</p>
