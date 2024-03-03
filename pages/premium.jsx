06694653a3399c0e8563312a3902dc07c90881f1
3nS4r28 <161999137+3nS4r28@users.noreply.github.com>
2024-03-03 14:28:31 +0300

    premium.jsx

--- pages/premium.jsx
+++ pages/premium.jsx
@@ -1,38 +1,3 @@
 
 export default function Premium() {
 
-    return (
-        <>
-            <div className="w-full my-10">
-            <div className="flex w-full justify-center">
-      <img width="400" src="/img/bck3.png" />
-      </div>
-                <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
-                    <i className="fal fa-stars text-amber-400 mr-2" />
-                    Premium
-                </h1>
-            </div>
-
-                return (
-        <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for DcBudy</h1>
-            <p className="text-sm text-white/30 mb-4">Last Update: 03/03/2024  🆕️</p>
-  <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the DcBudy bot, you need to define the necessary permissions when adding it to your discord server. <a className="text-white">(Administrator)</a></p>
-        <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/1205170061413982279/vote">DcBudy</a> bot when using certain commands from the bot commands.</p>
-        <div className="py-3"></div>
-        <p>⊳ Every user can use the DcBudy bot however they want, as long as they do not intend to harm.</p>
-        <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using DcBudy.</p>   
-        <div className="py-3"></div>
-        <p>⊳ DcBudy bot does not save any data based on users.</p>
-        <div className="py-3"></div>
-        <p>⊳ To use some of the commands of the DcBudy bot you need to buy Premium. To Buy <a className="text-white" href="https://discord.gg/QyvUUQ8rNK">CLİCK HERE</a> </p>
-        <div className="py-3"></div>
-        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
-    
-        </div>
-            </div>
-    );
-};
