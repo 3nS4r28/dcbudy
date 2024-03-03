0f02cc96e36a5a382067427b27c7be54c26a1559
3nS4r28 <161999137+3nS4r28@users.noreply.github.com>
2024-03-03 14:11:51 +0300

    premium.jsx

--- pages/premium.jsx
+++ pages/premium.jsx
@@ -14,7 +14,19 @@ export default function Partners() {
             </div>
 
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
-
+     <p>⊳ To use the DcBudy bot, you need to define the necessary permissions when adding it to your discord server. <a className="text-white">(Administrator)</a></p>
+        <div className="py-3"></div>
+        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/1205170061413982279/vote">DcBudy</a> bot when using certain commands from the bot commands.</p>
+        <div className="py-3"></div>
+        <p>⊳ Every user can use the DcBudy bot however they want, as long as they do not intend to harm.</p>
+        <div className="py-3"></div>
+        <p>⊳ You cannot use certain commands at the same time while using DcBudy.</p>   
+        <div className="py-3"></div>
+        <p>⊳ DcBudy bot does not save any data based on users.</p>
+        <div className="py-3"></div>
+        <p>⊳ To use some of the commands of the DcBudy bot you need to buy Premium. To Buy <a className="text-white" href="https://discord.gg/QyvUUQ8rNK">CLİCK HERE</a> </p>
+        <div className="py-3"></div>
+        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
                 </div>
                 <div className="py-10"></div>
         </>
