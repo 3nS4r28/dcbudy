27f0b36b8dc9c551ef041718d502a6ae9c706c9e
3nS4r28 <161999137+3nS4r28@users.noreply.github.com>
2024-03-05 18:54:53 +0300

    premium.jsx

--- pages/premium.jsx
+++ pages/premium.jsx
@@ -1,8 +1,8 @@
-export default function Pramium() {
+export default function Premium() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">DcBudy Premium</h1>
+            <h1 className="font-semibold text-3xl text-white/75">DcBudy PRO</h1>
   <div className="animateHeader text-white text-opacity-40 mt-5">
         <p>⊳ To use some of the commands of the DcBudy bot you need to buy Premium. To Buy <a className="text-white" href="https://discord.gg/QyvUUQ8rNK">CLİCK HERE</a> </p>
         <p>⊳ <a className="text-white" >Supported Money Types</a> </p>
